2ce136f28918683d8ad25e39ac28ea7e89d63c10
ndev92 <norman.dev92@yahoo.com>
2024-11-15 12:14:59 +1300

    Updated dotnet and stripe elements
    updated to use a more recent dotnet solution, update libraries, java script using stripe elements

--- server/dotnet/Controllers/PaymentsController.cs
+++ server/dotnet/Controllers/PaymentsController.cs
@@ -22,41 +22,45 @@ namespace server.Controllers
         [HttpGet("public-key")]
         public PublicKeyResponse PublicKey()
         {
-          // Fetches the public key so that the front end can
-          // initialize Stripe.js which is required for confirming
-          // the SetupIntent on the client.
-          return new PublicKeyResponse
-          {
-            PublicKey = this.options.Value.PublishableKey,
-          };
+            // Fetches the public key so that the front end can
+            // initialize Stripe.js which is required for confirming
+            // the SetupIntent on the client.
+            return new PublicKeyResponse
+            {
+                PublicKey = this.options.Value.PublishableKey,
+            };
         }
 
         [HttpPost("create-setup-intent")]
         public async Task<SetupIntent> CreateSetupIntent()
         {
-          // Here we create a customer, then create a SetupIntent.
-          // The SetupIntent is the object that keeps track of the
-          // Customer's intent to allow saving their card details
-          // so they can be charged later when the customer is no longer
-          // on your site.
+            // Here we create a customer, then create a SetupIntent.
+            // The SetupIntent is the object that keeps track of the
+            // Customer's intent to allow saving their card details
+            // so they can be charged later when the customer is no longer
+            // on your site.
 
-          // Create a customer
-          var options = new CustomerCreateOptions();
-          var service = new CustomerService();
-          var customer = await service.CreateAsync(options);
+            // Create a customer
+            var options = new CustomerCreateOptions();
+            var service = new CustomerService();
+            var customer = await service.CreateAsync(options);
 
-          // Create a setup intent, and return the setup intent.
-          var setupIntentOptions = new SetupIntentCreateOptions
-          {
-            Customer = customer.Id,
-          };
-          var setupIntentService = new SetupIntentService();
+            // Create a setup intent, and return the setup intent.
+            var setupIntentOptions = new SetupIntentCreateOptions
+            {
+                Customer = customer.Id,
+                AutomaticPaymentMethods = new SetupIntentAutomaticPaymentMethodsOptions
+                {
+                    Enabled = true,
+                },
+            };
+            var setupIntentService = new SetupIntentService();
 
-          // We're returning the full SetupIntent object here, but you could
-          // also just return the ClientSecret for the newly created
-          // SetupIntent as that's the only required data to confirm the
-          // SetupIntent on the front end with Stripe.js.
-          return await setupIntentService.CreateAsync(setupIntentOptions);
+            // We're returning the full SetupIntent object here, but you could
+            // also just return the ClientSecret for the newly created
+            // SetupIntent as that's the only required data to confirm the
+            // SetupIntent on the front end with Stripe.js.
+            return await setupIntentService.CreateAsync(setupIntentOptions);
         }
 
         [HttpPost("webhook")]
